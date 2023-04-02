91771e27751f14130a5126cf0796bc6b0394ac34
Rohith Saradhy <rohithsaradhy@gmail.com>
2023-04-01 17:57:15 -0700

    Do not use this file

--- firmware/generate_memoryHEX.tcl
+++ firmware/generate_memoryHEX.tcl
@@ -2,6 +2,8 @@
 # launch_runs impl_1 -to_step write_bitstream -jobs 10
 # write_cfgmem  -format hex -size 32 -interface SPIx1 -loadbit {up 0x00000000 "/home/rsaradhy/work/nexys_ddmtd_latest/Firmware/Nexys.runs/impl_1/main_v2.bit" } -force -file "/home/rsaradhy/work/nexys_ddmtd_latest/rpi_side/HEX_Files/2022_12_03_v2_032.hex"
 
-# S25FL256S
+# S25FL256S    xxxx0
 
 write_cfgmem  -format hex -size 32 -interface SPIx1 -loadbit {up 0x00000000 "/home/rsaradhy/work/nexys_ddmtd_github/firmware/project_folder/project_folder.runs/impl_1/main_v2.bit" } -force -file "/home/rsaradhy/work/nexys_ddmtd_github/rpi_side/HEX_Files/40MHz_6.25Delay_MAIN_v2_042.hex"
+
+write_cfgmem  -format hex -size 32 -interface SPIx1 -loadbit {up 0x00000000 "/home/rsaradhy/work/nexys_ddmtd_github/firmware/project_folder/project_folder.runs/impl_1/main_v2.bit" } -file "/home/rsaradhy/work/nexys_ddmtd_github/rpi_side/HEX_Files/160MHz_MAIN_v2_053.hex" -force
