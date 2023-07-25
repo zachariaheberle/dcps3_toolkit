63a9c853f8691186d28b4738f6a28bb279e1e1f2
Zachariah Eberle <zachariah.eberle@gmail.com>
2023-07-25 11:42:29 -0500

    Created plotting functions

--- analysis/plotDCPS.py
+++ analysis/plotDCPS.py
@@ -17,74 +17,151 @@ matplotlib.style.available
 # plt.rcParams['figure.figsize'] = [4, 3]
 plt.rcParams['figure.dpi'] = 300
 
-# Setting all the variables...
+# Setting default variables...
 N=100000  #Setting the DDMTD N
 freq="160"#in MHz #Setting the input clock frequency
-pll_config_folder="../rpi_side/PLL_Conf/"
 fig_save_folder = f"./dcps3Test/figures/N{N}/"
 data_save_folder = f"./dcps3Test/data/N{N}/"
-run_number = 1
 coarse_control = 0
 fine_control = 0
 stage4_tune = 2
-stage5_tune = 2
+stage5_tune = 3
 channel = 2
 run_name = f"chan{channel}_f{fine_control}_c{coarse_control}_s4{stage4_tune}_s5{stage5_tune}"
 
-dat = []
-for coarse_control in range(0,32,1):
-    try:
-    # for coarse_control in [0,1,2,4,8,16]:
-        # run_name = f"f_{fine_control}"
-        run_name = f"chan{channel}_f{fine_control}_c{coarse_control}_s4{stage4_tune}_s5{stage5_tune}"
-        print(f"{data_save_folder+run_name}_ddmtd1.txt")
-        df1 = pd.read_csv(f"{data_save_folder+run_name}_ddmtd1.txt",names=['edge1','ddmtd1'])
-        df2 = pd.read_csv(f"{data_save_folder+run_name}_ddmtd2.txt",names=['edge2','ddmtd2'])
-        df = pd.concat((df1,df2),axis=1)
-        # df
-
-        # df.ddmtd2.diff().iloc[:].plot()
-        # df.ddmtd1.diff().iloc[:].plot()
-
-
-        # Create a DDMTD Object for analysis of the data
-        data = ddmtd(df.iloc[:,:],channel=(1,2)) #creates a ddmtd object
-        #Setting the correct N can recalculating all the scalings
-        data.N = N  
-        # data.N = 1000000 
-        data.INPUT_FREQ = 160*10**6 #In Hz 
-        data.Recalc()
+def get_data(data_save_folder, run_name, fit=True, draw=False):
+    df1 = pd.read_csv(f"{data_save_folder+run_name}_ddmtd1.txt",names=['edge1','ddmtd1'])
+    df2 = pd.read_csv(f"{data_save_folder+run_name}_ddmtd2.txt",names=['edge2','ddmtd2'])
+    df = pd.concat((df1,df2),axis=1)
+    # Create a DDMTD Object for analysis of the data
+    data = ddmtd(df.iloc[2:],channel=(1,2), q=1) #creates a ddmtd object (fix 2: later)
+    data.N = N
+    data.INPUT_FREQ = 160*10**6 #In Hz 
+    data.Recalc()
+    if fit:
+        gauss_fit, _, _ = data.drawTIE(sep='',fit=True,draw=draw)
+        mean_val, std_dev = gauss_fit[1]*1000, abs(gauss_fit[2])*1000
+        return mean_val, std_dev
+    else:
         mean_val = np.mean(np.concatenate((data.TIE_rise,data.TIE_fall)))*data.MULT_FACT*1000
-        # print(
-        # # np.mean(data.TIE_fall)*data.MULT_FACT*1000,
-        # # np.mean(data.TIE_rise)*data.MULT_FACT*1000,
-        # np.mean(np.concatenate((data.TIE_rise,data.TIE_fall)))*data.MULT_FACT*1000
-        # )
+        return mean_val
 
-        dat.append((coarse_control,mean_val))
-    except FileNotFoundError:
-        print(f"Could not find {data_save_folder+run_name}_ddmtd1.txt")
-        continue
+def plot_coarse_consistency(data_save_folder):
+    f = plt.figure(figsize=(10,4))
+    f.subplots_adjust(wspace=0.3)
+    coarse_control = 0
+    fine_control = 0
+    stage4_tune = 2
+    stage5_tune = 3
+    for channel in range(2, 4, 1):
+        channel_data = []
+        for run in range(10):
+            run_data = []
+            for coarse_control in range(32):
+                print(f"Calculating coarse control: {coarse_control} run: {run} channel: {channel}")
+                run_name = f"chan{channel}_f{fine_control}_c{coarse_control}_s4{stage4_tune}_s5{stage5_tune}_run{run}"
+                mean_val, std_dev = get_data(data_save_folder, run_name)
+                run_data.append((coarse_control, mean_val, std_dev))
+            run_data = np.asarray(run_data)
+            x = run_data.T[0]
+            y = -1*(run_data.T[1]+200)%3125
+            yerr = -1*(run_data.T[2]+200)%3125
+            y = y-y[0]
 
-dat = np.asarray(dat)
-x = dat.T[0]
-y = -1*(dat.T[1]+200)%3125
+            popt,pcov = np.polyfit(x,y,1,cov=True,w=1/yerr**2)
+            p_e = np.sqrt(np.diag(pcov))
 
-y = y-y[0]
+            channel_data.append((run, popt[0], p_e[0]))
+    
+        channel_data = np.asarray(channel_data)
+        x = channel_data.T[0]
+        y = channel_data.T[1]
+        yerr = channel_data.T[2]
 
-# x = x[:(len(x) - 1)]
-# y = y[:(len(y) - 1)]
+        weighted_mean = np.average(y, weights=1/yerr**2)
+        _sig = np.std(y)
 
-f,ax = plt.subplots()
-# fit
-popt,pcov = np.polyfit(x,y,1,cov=True)
-p_e = np.sqrt(np.diag(pcov))
-# ax.plot(x,popt[0]*x+popt[1],color="r",linestyle='--',label=f"Channel {channel} \n{popt[0]:4.3}+/- {p_e[0]:4.2} [ps/step]")
+        # popt,pcov = np.polyfit(x,y,0,cov="unscaled",w=1/yerr**2)
+        # p_e = np.sqrt(np.diag(pcov))
 
-plt.scatter(x,y,s=6)
+        # weighted_mean = popt[0]
+        # _sig = p_e[0]
 
-ax.set_ylim([0, 248])
-ax.set_ylabel("Delay [ps]")
-# ax.set_xlabel("Coarse Step")
-ax.legend(loc="upper left",fontsize=10)
-ax.set_title(f"Coarse Delay Channel {channel}: {stage4_tune} {stage5_tune}")
+
+        ax = f.add_subplot(int(f"12{channel-1}"))
+        ax.grid(True, alpha=0.5)
+        ax.axhline(y=weighted_mean, color='black',linewidth=1, linestyle='-.',label=f"Mean Delay Slope All Runs\n{weighted_mean:4.3}+/-{_sig:4.2} [ps/step]")
+        ax.fill_between(x, weighted_mean+_sig, weighted_mean-_sig, color='orange', alpha=.5, label="\u03c3 All Runs")
+        ax.errorbar(x, y, yerr=yerr, fmt='r.', ecolor="black", capsize=2, label=f"Delay per Coarse Step")
+
+        ax.set_ylim([7.1, 7.22])
+
+        ax.set_ylabel("Delay per Coarse Step [ps/step]")
+        ax.set_xlabel("Run Number")
+        ax.set_xticks(range(10))
+        ax.set_xticklabels(range(10))
+        ax.legend(loc="upper left",fontsize=8)
+        ax.set_title(f"Coarse Delay Consistency Check\nChannel {channel}: {stage4_tune} {stage5_tune}")
+    plt.savefig("dcps3Test/figures/dcps3_coarse_consistency_test2.png", dpi=300, facecolor="#FFFFFF")
+    plt.close()
+
+def plot_fine_consistency(data_save_folder):
+    f = plt.figure(figsize=(10,4))
+    f.subplots_adjust(wspace=0.3)
+    coarse_control = 0
+    fine_control = 0
+    stage4_tune = 2
+    stage5_tune = 3
+    for channel in range(2, 4, 1):
+        channel_data = []
+        for run in range(10):
+            run_data = []
+            for fine_control in range(66):
+                print(f"Calculating fine control: {fine_control} run: {run} channel: {channel}")
+                run_name = f"chan{channel}_f{fine_control}_c{coarse_control}_s4{stage4_tune}_s5{stage5_tune}_run{run}"
+                mean_val, std_dev = get_data(data_save_folder, run_name)
+                run_data.append((fine_control, mean_val, std_dev))
+            run_data = np.asarray(run_data)
+            x = run_data.T[0]
+            y = -1*(run_data.T[1]+200)%3125
+            yerr = -1*(run_data.T[2]+200)%3125
+            y = y-y[0]
+
+            popt,pcov = np.polyfit(x,y,1,cov=True,w=1/yerr**2)
+            p_e = np.sqrt(np.diag(pcov))
+
+            channel_data.append((run, popt[0], p_e[0]))
+    
+        channel_data = np.asarray(channel_data)
+        x = channel_data.T[0]
+        y = channel_data.T[1]
+        yerr = channel_data.T[2]
+
+        weighted_mean = np.average(y, weights=1/yerr**2)
+        _sig = np.std(y)
+
+        # popt,pcov = np.polyfit(x,y,0,cov="unscaled",w=1/yerr**2)
+        # p_e = np.sqrt(np.diag(pcov))
+
+        # weighted_mean = popt[0]
+        # _sig = p_e[0]
+
+        ax = f.add_subplot(int(f"12{channel-1}"))
+        ax.grid(True, alpha=0.5)
+        ax.axhline(y=weighted_mean, color='black',linewidth=1, linestyle='-.',label=f"Mean Delay Slope All Runs\n{weighted_mean:4.3}+/-{_sig:4.2} [ps/step]")
+        ax.fill_between(x, weighted_mean+_sig, weighted_mean-_sig, color='orange', alpha=.5, label="\u03c3 All Runs")
+        ax.errorbar(x, y, yerr=yerr, fmt='r.', ecolor="black", capsize=2, label=f"Delay per Fine Step")
+
+        ax.set_ylim([0.265, 0.280])
+        
+        ax.set_ylabel("Delay per Fine Step [ps/step]")
+        ax.set_xlabel("Run Number")
+        ax.set_xticks(range(10))
+        ax.set_xticklabels(range(10))
+        ax.legend(loc="upper left",fontsize=8)
+        ax.set_title(f"Fine Delay Consistency Check\nChannel {channel}: {stage4_tune} {stage5_tune}")
+    plt.savefig("dcps3Test/figures/dcps3_fine_consistency_test2.png", dpi=300, facecolor="#FFFFFF")
+    plt.close()
+
+plot_coarse_consistency(f"./dcps3Test/data/N{N}_coarse/")
+plot_fine_consistency(f"./dcps3Test/data/N{N}_fine/")
