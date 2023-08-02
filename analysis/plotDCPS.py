fc2cdc9b329fdab860d72a44987b29799dca9433
Zachariah Eberle <zachariah.eberle@gmail.com>
2023-08-02 11:34:09 -0500

    Added fig folder checks; options to plot all runs

--- analysis/plotDCPS.py
+++ analysis/plotDCPS.py
@@ -45,7 +45,7 @@ def get_data(data_save_folder, run_name, fit=True, draw=False):
     data.INPUT_FREQ = 160*10**6 #In Hz 
     data.Recalc()
     if fit:
-        gauss_fit, _, count = data.drawTIE(sep='',fit=True,draw=draw)
+        gauss_fit, _, count = data.drawTIE(sep='',fit=True,draw=False)
         mean_val, std_dev = gauss_fit[1]*1000, abs(gauss_fit[2])*1000
         return mean_val, std_dev / np.sqrt(count)
     else:
@@ -53,17 +53,17 @@ def get_data(data_save_folder, run_name, fit=True, draw=False):
         return mean_val
 
 def adjust_offsets(y, offset_8, offset_16, offset_24):
-        new_vals = []
-        for i, val in enumerate(y):
-            if i < 8:
-                new_vals.append(val)
-            elif 8 <= i < 16:
-                new_vals.append(val - offset_8)
-            elif 16 <= i < 24:
-                new_vals.append(val - offset_16)
-            elif 24 <= i:
-                new_vals.append(val - offset_24)
-        return np.asarray(new_vals)
+    new_vals = []
+    for i, val in enumerate(y):
+        if i < 8:
+            new_vals.append(val)
+        elif 8 <= i < 16:
+            new_vals.append(val + offset_8)
+        elif 16 <= i < 24:
+            new_vals.append(val + offset_16)
+        elif 24 <= i:
+            new_vals.append(val + offset_24)
+    return np.asarray(new_vals)
 
 def find_offsets(x, y, yerr):
 
@@ -88,9 +88,7 @@ def find_offsets(x, y, yerr):
     step = 0
     min_err = get_slope_error(x, y, yerr)
     while ideal == False:
-        # print(f"Power: {power}, Step: {step}")
-        # print(f"Offset 8: {offset_8}, Offset 16: {offset_16}, Offset 24: {offset_24}")
-        # print(f"Min Error: {min_err}\n")
+        #print(f"Min Error: {min_err}\n")
         if step == 0:
             if check_offset(y, offset_8 - 10**(-power), offset_16, offset_24, min_err)[0] == True:
                 min_err = check_offset(y, offset_8 - 10**(-power), offset_16, offset_24, min_err)[1]
@@ -132,6 +130,8 @@ def find_offsets(x, y, yerr):
                     step += 1
         else:
             ideal = True
+            #print(f"y is {y}")
+            #print(f"Error should be {min_err:.3f}, Offsets are {offset_8:.3f}, {offset_16:.3f}, {offset_24:.3f}")
             return offset_8, offset_16, offset_24 
 
 
@@ -139,64 +139,97 @@ def find_offsets(x, y, yerr):
 
 
 
-def plot_coarse_consistency(data_save_folder, figure_save_folder):
+def plot_coarse_consistency(data_save_folder, figure_save_folder, plot_all_runs=False):
     f = plt.figure(figsize=(10,4))
     f.subplots_adjust(wspace=0.3)
     coarse_control = 0
     fine_control = 0
     stage4_tune = 2
     stage5_tune = 3
+
+    try:
+        os.makedirs(figure_save_folder)
+    except FileExistsError:
+        pass
+    if plot_all_runs:
+        try:
+            os.makedirs(f"{figure_save_folder}/coarse_consistency_plots")
+        except FileExistsError:
+            pass
+
     for channel in range(2, 4, 1):
         channel_data = []
+        offset_data = []
         for run in range(10):
             run_data = []
             for coarse_control in range(32):
                 print(f"Calculating coarse control: {coarse_control} run: {run} channel: {channel}")
-                run_name = f"chan{channel}_f{fine_control}_c{coarse_control}_s4{stage4_tune}_s5{stage5_tune}_run{run}"
+                run_name = f"/chan{channel}_f{fine_control}_c{coarse_control}_s4{stage4_tune}_s5{stage5_tune}_run{run}"
                 mean_val, std_err = get_data(data_save_folder, run_name)
                 run_data.append((coarse_control, mean_val, std_err))
+
             run_data = np.asarray(run_data)
             x = run_data.T[0]
             y = -1*(run_data.T[1]+200)%3125
             yerr = run_data.T[2]
             y = y-y[0]
 
-            # ax = f.add_subplot(121)
-
-            # popt,pcov = np.polyfit(x,y,1,cov=True,w=1/yerr**2)
-            # p_e = np.sqrt(np.diag(pcov))
-
-            # ax.plot(x, popt[0]*x+popt[1],color="b",linestyle='--',label=f"Channel {channel} \n{popt[0]:4.3}+/- {p_e[0]:4.2} [ps/step]")
-            # ax.errorbar(x, y, yerr=yerr, fmt='r.', ecolor="black", capsize=2, label=f"Delay per Coarse Step")
-            # ax.set_title("Before Y adjustment")
-            # ax.legend()
-            #if run == 0:
             offset_8, offset_16, offset_24 = find_offsets(x, y, yerr)
-            y = adjust_offsets(y, offset_8, offset_16, offset_24)
 
-            #ax = f.add_subplot(122)
+            channel_data.append((x, y, yerr, run))
+            offset_data.append((offset_8, offset_16, offset_24))
+
+        offset_data = np.asarray(offset_data)
+
+        offset_8 = np.mean(offset_data.T[0])
+        offset_16 = np.mean(offset_data.T[1])
+        offset_24 = np.mean(offset_data.T[2])
+
+        # figg, ax = plt.subplots()
+        # ax.axhline(offset_8, color='black',linewidth=1, linestyle='-.')
+        # ax.axhline(offset_16, color='black',linewidth=1, linestyle='-.')
+        # ax.axhline(offset_24, color='black',linewidth=1, linestyle='-.')
+        # ax.fill_between(x, offset_8+offset_8_err, offset_8-offset_8_err, color='orange', alpha=.5)
+        # ax.fill_between(x, offset_16+offset_16_err, offset_16-offset_16_err, color='orange', alpha=.5)
+        # ax.fill_between(x, offset_24+offset_24_err, offset_24-offset_24_err, color='orange', alpha=.5)
+        # ax.scatter(range(10), offset_data.T[0], c="red", label=f"Offset 8")
+        # ax.scatter(range(10), offset_data.T[1], c="green", label=f"Offset 16")
+        # ax.scatter(range(10), offset_data.T[2], c="blue", label=f"Offset 24")
+        # ax.set_xlim(-1, 11)
+        # ax.legend(loc="best")
+        # plt.savefig(f"{figure_save_folder}/test.png", dpi=300, facecolor="#FFFFFF")
+        # plt.close(figg)
+        # return
+
+        slope_data = []
+        for x, y, yerr, run in channel_data:
+            #offset_8, offset_16, offset_24 = offset_data[run]
+
+            y = adjust_offsets(y, offset_8, offset_16, offset_24)
 
             popt,pcov = np.polyfit(x,y,1,cov=True,w=1/yerr**2)
             p_e = np.sqrt(np.diag(pcov))
 
-            # fig, ax = plt.subplots()
+            if plot_all_runs:
+                fig, ax = plt.subplots()
 
-            # ax.plot(x, popt[0]*x+popt[1],color="b",linestyle='--',label=f"Channel {channel} \n{popt[0]:4.3}+/- {p_e[0]:4.2} [ps/step]")
-            # ax.errorbar(x, y, yerr=yerr, fmt='r.', ecolor="black", capsize=2, label=f"Delay per Coarse Step")
-            # ax.set_title(f"Coarse Delay Consistency Check\nChannel {channel}: {stage4_tune} {stage5_tune}, Run {run}")
-            # ax.set_xlabel("Coarse Step")
-            # ax.set_ylabel("Delay [ps]")
-            # ax.legend()
-            # ax.set_ylim([-5, 250])
-            # plt.savefig(f"dcps3Test/figures/coarse_comparison/single_y_adjust/chan{channel}_run{run}.png")
-            # plt.close(fig)
+                ax.plot(x, popt[0]*x+popt[1],color="b",linestyle='--',label=f"Channel {channel} \n{popt[0]:4.3}+/- {p_e[0]:4.2} [ps/step]")
+                ax.errorbar(x, y, yerr=yerr, fmt='r.', ecolor="black", capsize=2, label=f"Delay per Coarse Step")
+                ax.set_title(f"Coarse Delay\nChannel {channel}: {stage4_tune} {stage5_tune} | Run {run}")
+                ax.set_xlabel("Coarse Step")
+                ax.set_ylabel("Delay [ps]")
+                ax.legend()
+                ax.set_ylim([-5, 250])
 
-            channel_data.append((run, popt[0], p_e[0]))
+                plt.savefig(f"{figure_save_folder}/coarse_consistency_plots/coarse_delay_chan{channel}_run{run}.png", dpi=300, facecolor="#FFFFFF")
+                plt.close(fig)
+            
+            slope_data.append((run, popt[0], p_e[0]))
     
-        channel_data = np.asarray(channel_data)
-        x = channel_data.T[0]
-        y = channel_data.T[1]
-        yerr = channel_data.T[2]
+        slope_data = np.asarray(slope_data)
+        x = slope_data.T[0]
+        y = slope_data.T[1]
+        yerr = slope_data.T[2]
 
         # weighted_mean = np.average(y, weights=1/yerr**2)
         # err = weighted_std_dev(weighted_mean, y, 1/yerr**2)
@@ -222,9 +255,8 @@ def plot_coarse_consistency(data_save_folder, figure_save_folder):
         ax.set_xticklabels(range(10))
         ax.legend(loc="upper left",fontsize=8)
         ax.set_title(f"Coarse Delay Consistency Check\nChannel {channel}: {stage4_tune} {stage5_tune}")
-    plt.savefig(f"{figure_save_folder}/dcps3_coarse_consistency_test.png", dpi=300, facecolor="#FFFFFF")
+    plt.savefig(f"{figure_save_folder}/dcps3_coarse_consistency.png", dpi=300, facecolor="#FFFFFF")
     plt.close()
-
 
 def plot_coarse_cell_consistency(data_save_folder, figure_save_folder):
     f = plt.figure(figsize=(10,24))
@@ -233,6 +265,12 @@ def plot_coarse_cell_consistency(data_save_folder, figure_save_folder):
     fine_control = 0
     stage4_tune = 2
     stage5_tune = 3
+
+    try:
+        os.makedirs(figure_save_folder)
+    except FileExistsError:
+        pass
+
     for channel in range(2, 4, 1):
         channel_data = []
         for i, coarse_control in enumerate([0, 1, 2, 4, 8, 16]):
@@ -284,15 +322,24 @@ def plot_coarse_cell_consistency(data_save_folder, figure_save_folder):
             ax.set_title(f"Coarse Delay Cell Consistency Check\nChannel {channel}: {stage4_tune} {stage5_tune}\nCell {coarse_control}")
     plt.savefig(f"{figure_save_folder}/dcps3_coarse_cell_consistency_test.png", dpi=300, facecolor="#FFFFFF")
 
-
-
-def plot_fine_consistency(data_save_folder, figure_save_folder):
+def plot_fine_consistency(data_save_folder, figure_save_folder, plot_all_runs=False):
     f = plt.figure(figsize=(10,4))
     f.subplots_adjust(wspace=0.3)
     coarse_control = 0
     fine_control = 0
     stage4_tune = 2
     stage5_tune = 3
+
+    try:
+        os.makedirs(figure_save_folder)
+    except FileExistsError:
+        pass
+    if plot_all_runs:
+        try:
+            os.makedirs(f"{figure_save_folder}/fine_consistency_plots")
+        except FileExistsError:
+            pass
+
     for channel in range(2, 4, 1):
         channel_data = []
         for run in range(10):
@@ -308,15 +355,37 @@ def plot_fine_consistency(data_save_folder, figure_save_folder):
             yerr = run_data.T[2]
             y = y-y[0]
 
+            channel_data.append((x, y, yerr, run))
+
+            # channel_data.append((run, popt[0], p_e[0]))
+    
+        slope_data = []
+        for x, y, yerr, run in channel_data:
+
             popt,pcov = np.polyfit(x,y,1,cov=True,w=1/yerr**2)
             p_e = np.sqrt(np.diag(pcov))
 
-            channel_data.append((run, popt[0], p_e[0]))
+            if plot_all_runs:
+                fig, ax = plt.subplots()
+
+                ax.grid(True)
+                ax.plot(x, popt[0]*x+popt[1],color="b",linestyle='--',label=f"Channel {channel} \n{popt[0]:4.3}+/- {p_e[0]:4.2} [ps/step]")
+                ax.errorbar(x, y, yerr=yerr, fmt='r.', ecolor="black", capsize=2, label=f"Delay per Coarse Step")
+                ax.set_title(f"Fine Delay\nChannel {channel}: {stage4_tune} {stage5_tune} | Run {run}")
+                ax.set_xlabel("Fine Step")
+                ax.set_ylabel("Delay [ps]")
+                ax.legend()
+                ax.set_ylim([-0.5, 20])
+
+                plt.savefig(f"{figure_save_folder}/fine_consistency_plots/coarse_delay_chan{channel}_run{run}.png", dpi=300, facecolor="#FFFFFF")
+                plt.close(fig)
+            
+            slope_data.append((run, popt[0], p_e[0]))
     
-        channel_data = np.asarray(channel_data)
-        x = channel_data.T[0]
-        y = channel_data.T[1]
-        yerr = channel_data.T[2]
+        slope_data = np.asarray(slope_data)
+        x = slope_data.T[0]
+        y = slope_data.T[1]
+        yerr = slope_data.T[2]
 
         # weighted_mean = np.average(y, weights=1/yerr**2)
         # err = weighted_std_dev(weighted_mean, y, 1/yerr**2)
@@ -352,6 +421,12 @@ def plot_fine_cell_single(data_save_folder, figure_save_folder):
     stage4_tune = 2
     stage5_tune = 3
     run = 0
+
+    try:
+        os.makedirs(figure_save_folder)
+    except FileExistsError:
+        pass
+
     for channel in range(2, 3, 1):
         run_data = []
         for fine_control in range(66):
@@ -398,6 +473,12 @@ def plot_fine_cell_consistency(data_save_folder, figure_save_folder):
     fine_control = 0
     stage4_tune = 2
     stage5_tune = 3
+
+    try:
+        os.makedirs(figure_save_folder)
+    except FileExistsError:
+        pass
+
     for channel in range(2, 4, 1):
         for i, fine_control in enumerate(range(66)):
             run_data = []
@@ -445,8 +526,7 @@ def plot_fine_cell_consistency(data_save_folder, figure_save_folder):
             ax.set_title(f"Fine Delay Cell Consistency Check\nChannel {channel}: Fine Cell {fine_control}")
     plt.savefig(f"{figure_save_folder}/dcps3_fine_cell_consistency_test.pdf", dpi=100, facecolor="#FFFFFF")
 
-
-def plot_fine_cell_relative_consistency_single(data_save_folder, figure_save_folder):
+def plot_fine_cell_relative_consistency_single(data_save_folder, figure_save_folder, plot_all_runs=False):
     f = plt.figure(figsize=(16,4))
     f.subplots_adjust(top=0.85, bottom=0.15, left=0.05, right=0.96)
     coarse_control = 0
@@ -454,6 +534,12 @@ def plot_fine_cell_relative_consistency_single(data_save_folder, figure_save_fol
     stage4_tune = 2
     stage5_tune = 3
     run = 0
+
+    try:
+        os.makedirs(figure_save_folder)
+    except FileExistsError:
+        pass
+
     for channel in range(2, 3, 1):
         run_data = []
         for fine_control in range(66):
@@ -500,10 +586,9 @@ def plot_fine_cell_relative_consistency_single(data_save_folder, figure_save_fol
     plt.savefig(f"{figure_save_folder}/dcps3_fine_cell_relative_consistency_test.png", dpi=300, facecolor="#FFFFFF")
 
 
-plot_coarse_consistency(f"./dcps3Test/data/board1/N{N}_coarse", f"./dcps3Test/figure/board1")
-plot_fine_consistency(f"./dcps3Test/data/board1/N{N}_fine", f"./dcps3Test/figure/board1")
-plot_coarse_cell_consistency(f"./dcps3Test/data/board1/N{N}_coarse", f"./dcps3Test/figure/board1")
-plot_fine_cell_consistency(f"./dcps3Test/data/board1/N{N}_fine_cell", f"./dcps3Test/figure/board1")
+#plot_coarse_consistency(f"./dcps3Test/data/board1/N{N}_coarse", f"./dcps3Test/figures/board1", True)
+plot_fine_consistency(f"./dcps3Test/data/board1/N{N}_fine", f"./dcps3Test/figures/board1", True)
+#plot_coarse_cell_consistency(f"./dcps3Test/data/board1/N{N}_coarse", f"./dcps3Test/figures/board1")
+#plot_fine_cell_consistency(f"./dcps3Test/data/board1/N{N}_fine_cell", f"./dcps3Test/figures/board1")
 #plot_fine_cell_relative_consistency_single(f"./dcps3Test/data/board1/N{N}_fine/")
 #plot_fine_cell_set_consistency_single(f"./dcps3Test/data/board1/N{N}_fine_cell_set/")
-#print(check_for_bug())
